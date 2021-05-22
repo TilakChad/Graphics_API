2068761349305eb0d8619c80723ee073118511c8
Tilak Chad <tilakchad111@gmail.com>
2021-05-22 14:17:09 +0545

    Assigned different color for illuminated pixels

--- src/shaders/fragment_shader.fs
+++ src/shaders/fragment_shader.fs
@@ -6,7 +6,7 @@ uniform int color_code;
 void main()
 {
 	if(color_code == 0)
-		color = vec4(0.6f,0.6f,0.6f,1.0f);
+		color = vec4(0.65f,0.65f,0.65f,1.0f);
 	else if (color_code == 1)
 		color = vec4(1.0f,0.0f,0.0f,1.0f);
 	else 
