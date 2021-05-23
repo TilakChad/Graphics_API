ffdfc49d522e1268c77ecece8f337cb78d01cae8
Tilak Chad <tilakchad111@gmail.com>
2021-05-23 20:13:28 +0545

    Moved some type definition out

--- includes/renderer.h
+++ includes/renderer.h
@@ -5,75 +5,12 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
-
-typedef struct Point
-{
-	float x;
-	float y;
-} Point;
-
+#include "abstract.h"
 typedef struct shader
 {
 	GLuint vertex_shader;
 	GLuint fragment_shader;
 } shader;
-
-typedef struct Renderer
-{
-	// Could've made a different struct instead of using anonymous.. but its ok. It was a later addition .. so didn't consider it changing
-
-	struct
-	{
-		bool contain_VBO;
-		GLuint grid_VBO;
-		GLuint grid_VAO;
-	} grid;
-
-	struct
-	{
-		bool contain_VBO;
-		GLuint origin_VBO;
-		GLuint origin_VAO;
-	} origin;
-
-	GLuint shader_program;
-	GLuint vertices_count[3];
-	// ok let's include them here too
-	struct
-	{
-		bool contain_VBO; // Check this flag before deleting the bound VBO.. be safe.. Its functional
-		GLuint plot_VBO;
-		GLuint plot_VAO; // Not going to use element array now .. And using the same set of shaders for now
-	} plot;
-
-	// it's verbose but its ok for now 
-	enum
-	{
-		RENDER_ALL, RENDER_NO_GRIDS, RENDER_NO_ORIGIN, RENDER_NO_PIXELS, RENDER_NO_GRIDS_AND_ORIGINS
-	} render_type;
-} Renderer;
-
-typedef struct frameBuffer
-{
-	GLfloat origin_x;
-	GLfloat origin_y;
-	/*Point scale_factor;*/
-	// scale factor is going to be same.. i.e every pixel will be perfect square
-	GLfloat scale_factor;
-	GLfloat aspect_ratio;
-	struct
-	{
-		unsigned int size;
-		unsigned int capacity;
-		Point* points; 
-	} plotted_points;
-
-	struct
-	{
-		float left, right, top, bottom;
-		int left_coord, right_coord, top_coord, bottom_coord;
-	}plot_info;
-}frameBuffer;
 
 
 int initialize_renderer(Renderer* render_engine,frameBuffer*frame_buffer, int width, int height);
@@ -87,6 +24,7 @@ int load_shader_from_file(struct shader* shaders, const char* vertex_shader_path
 // each pixel is a square whose dimension depends upon the scale factor and the height of the 
 // window
 
+// Some functions shouldn't be provided to the API user directly
 void setPixel(frameBuffer* frame_buffer, Point p);
 
 // Reset the given pixel
